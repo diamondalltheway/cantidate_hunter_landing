2b5a8e75e3038c3517895939076e0111052af36a
diamondalltheway <hunter@toplobster.io>
2022-08-20 16:01:45 -0500

    Connect Landing Page form to Slack Webhooks

---
+++ webhook.js
@@ -0,0 +1,34 @@
+function getMiniData() {
+  let email = document.getElementById('emailMini').value;
+  let obj = {email: email};
+  sendLead(obj);
+}
+
+function getFormData() {
+  let firstName = document.getElementById('first-name').value;
+  let lastName = document.getElementById('last-name').value;
+  let companyName = document.getElementById('company').value;
+  let email = document.getElementById('first-name').value;
+  let phoneNumber = document.getElementById('phone-number').value;
+  let comments = document.getElementById('message').value;
+
+  let obj = {
+    firstName: firstName,
+    lastName: lastName,
+    companyName: companyName,
+    email: email,
+    phoneNumber: phoneNumber,
+    comments: comments,
+  }
+  sendLead(obj);
+}
+
+function sendLead(obj) {
+  const url = 'https://hooks.slack.com/services/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';
+  const xhr = new XMLHttpRequest()
+  let payload = {}
+      payload.text = JSON.stringify(obj);
+
+  xhr.open("POST", url);
+  xhr.send(JSON.stringify(payload))
+}
